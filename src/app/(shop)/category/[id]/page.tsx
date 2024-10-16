de454652d826df798450b8bd5b69833cfcd090ec
Brandon Castillo <brajoel98@gmail.com>
2024-10-15 22:42:40 -0700

    feat(pages): :sparkles: create pages and routes of mens, girls and kids

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,6 +1,16 @@
 import { monsserat } from "@/fonts";
+import { notFound } from "next/navigation";
 
-export default function CategoryPage() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function CategoryPage({ params }: Props) {
+  const sitesAllowed = ["girls", "mens", "kids"];
+  if (!sitesAllowed.includes(params.id)) notFound();
+
   return (
     <main>
       <h1 className={monsserat.className}>Category page</h1>
