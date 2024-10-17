8a09d9e65cb1bf597669e880c5f0bcbc155f8dc9
Brandon Castillo <brajoel98@gmail.com>
2024-10-16 21:54:26 -0700

    feat(components): :sparkles: ProductGridItem and ProductItem created

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,14 @@
 import { Title } from "@/components";
+import { ProductGrid } from "@/components/products";
+import { initialData } from "@/seed";
+
+const products = initialData.products;
 
 export default function ShopPage() {
-  return <Title title="Shop" subtitle="Shop page" className="mb-2" />;
+  return (
+    <>
+      <Title title="Shop" subtitle="Shop page" className="mb-2" />
+      <ProductGrid products={products} />
+    </>
+  );
 }
