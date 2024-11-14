b421bbffab546fa695bf116f00d67aa42e67b88f
Brandon Castillo <brajoel98@gmail.com>
2024-11-13 22:14:06 -0800

    feat(components): :sparkles: create Footer component

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,5 +3,9 @@ export default function LoginLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="min-h-screen bg-purple-400">{children}</main>;
+  return (
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
+    </main>
+  );
 }
