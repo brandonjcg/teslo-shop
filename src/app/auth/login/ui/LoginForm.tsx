932456405ac2ab4312e8bb74d5a48c8a9a755650
Brandon Castillo <brajoel98@gmail.com>
2024-12-04 16:33:07 -0800

    feat(auth): :passport_control: init auth config

---
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import { useFormState } from 'react-dom';
+import { authenticate } from '@/actions';
+
+export const LoginForm = () => {
+  const [state, dispatch] = useFormState(authenticate, undefined);
+
+  return (
+    <form action={dispatch} className="flex flex-col">
+      <label htmlFor="email">Email</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        name="email"
+      />
+
+      <label htmlFor="email">Password</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        name="password"
+      />
+
+      <button className="btn-primary">Login</button>
+
+      {/* divisor line */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link href="/auth/new-account" className="btn-secondary text-center">
+        Create new account
+      </Link>
+    </form>
+  );
+};
---
+++ src/auth.config.ts
@@ -0,0 +1,31 @@
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+export const authConfig = {
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/new-account',
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+
+        console.log('credentials', credentials);
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
+
+export const { signIn, signOut } = NextAuth(authConfig);
