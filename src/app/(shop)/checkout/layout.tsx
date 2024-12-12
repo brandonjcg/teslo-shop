afe96ec7158f8ef51ebe316968f11f4ff6a1435f
Brandon Castillo <brajoel98@gmail.com>
2024-12-12 06:53:30 -0800

    feat(routes): :passport_control: protected routes with session

---
+++ src/app/(shop)/checkout/layout.tsx
@@ -0,0 +1,13 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@/auth.config';
+
+export default async function CheckoutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  if (!session?.user) redirect('/auth/login?redirect=/checkout/address');
+
+  return <>{children}</>;
+}
